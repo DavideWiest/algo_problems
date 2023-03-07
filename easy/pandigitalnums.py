8153d3794fef1aabe2681b8e33aecf066cc273af
Davide R. Wiest <davide.wiest2@gmail.com>
2023-03-07 10:30:46 +0100

    Update pandigitalnums.py

--- easy/pandigitalnums.py
+++ easy/pandigitalnums.py
@@ -1,30 +1,30 @@
 import math
 
-def digitsOfNum(n) -> list:
-    "find digits of a number"
+DIGITS = [n for n in range(1,10)]
+
+def numDigits(n):
+    if n < 0: n *= -1
+    if n < 10: return [n]
+    mx = 1
     digits = []
-    # use remainder division
-    div = 1
-    while n / div > 1:
-        digits.append(math.floor(n % div))
-        div *= 10
-    return digits
+    while mx<n:
+        mx *= 10
+
+    while n>1:
+        mx /= 10
+        digits.append(math.floor(n/mx))
+        n -= mx * digits[-1]
+
+        return digits
 
 def isPandigital(m1,m2,p) -> bool:
     "True if arguments have all digits from 1 to 9"
-
-    digits = [n for n in range(1,10)]
-
-    for d in digitsOfNum(m1):
-        digits.remove(d)
     
-    for d in digitsOfNum(m2):
-        digits.remove(d)
+    combinedDigits = digitsOfNum(m1) + digitsOfNum(m2) + digitsOfNum(p)
+    if len(combinedDigits) != 10: return False
+    
 
-    for d in digitsOfNum(p):
-        digits.remove(d)
-
-    return digits == []
+    return all(d in combinedDigits for d in DIGITS)
 
 def findAllPandigital() -> list:
     "find all pandigital numbers until some limit"
@@ -39,12 +39,11 @@ def findAllPandigital() -> list:
         for m2 in range(m2_lo, m2_hi):
             if isPandigital(m1,m2,m1*m2):
                 pandigitals.append(m1*m2)
-
-
+       
+    return pandigitals
 
 if __name__ == '__main__':
-    # print(sum(findAllPandigital()))
-    print(digitsOfNum(199))
+    print(sum(findAllPandigital()))
 
 # m1 * m2 * p has 10 digits
 # check for pandigital only if thats true for the nums
